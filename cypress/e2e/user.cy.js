6435efb8fceca53f699c0c24353db0c4d6b41823
Felipe-Recova <feliperec3@gmail.com>
2025-04-30 23:01:17 -0300

    User update scenario adjusts

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -26,93 +26,29 @@ describe('template spec', () => {
   it.only('User info Update', () => {
     cy.visit('/auth/login')
   
-    cy.get(selectorsList.usernameField)
-      .type(userData.userSuccess.username)
-
-    cy.get(selectorsList.passwordField)
-      .type(userData.userSuccess.password)
-
-    cy.get(selectorsList.loginButton)
-      .click()
-
-    cy.location('pathname')
-      .should('equal','/web/index.php/dashboard/index')
-
-    cy.get(selectorsList.dashboardGrid)
-      .should('be.visible')
-
-    cy.get(selectorsList.myInfoButton)
-      .click()
-
-    cy.get(selectorsList.headerMyInfo)
-      .contains('PIM')
-      .should('be.visible')
-    
-    cy.get(selectorsList.firstNameField)
-      .clear()
-      .type('Felipe')
-
-    cy.get(selectorsList.middleNameField)
-      .clear()
-      .type('Recova')
-
-    cy.get(selectorsList.lastNameField)
-      .clear()
-      .type('Castilhos')
-
-    cy.get(selectorsList.genericField)
-      .eq(3)
-      .clear()
-      .type('Recovera')
-
-    cy.get(selectorsList.genericField)
-      .eq(4)
-      .clear()
-      .type('77742')
-
-    cy.get(selectorsList.genericField)
-      .eq(5)
-      .clear()
-      .type('ABC_123456-xyz')
-
-    cy.get(selectorsList.dateField)
-      .eq(0)
-      .clear()
-      .type('2030-12-31')   
-
-    cy.get(selectorsList.dateCloseButton)
-      .click()
-
-    cy.get(selectorsList.nationalitySelect)
-      .eq(0)
-      .click()
-      
-    cy.get('.oxd-select-dropdown')
-      .contains('Brazilian')
-      .click()
-
-    cy.get(selectorsList.maritalStatusSelect)
-      .eq(1)
-      .click()
-
-    cy.get('.oxd-select-dropdown')
-      .contains('Married')
-      .click()
-
-    cy.get(selectorsList.dateField)
-      .eq(1)
-      .clear()
-      .type('1983-07-27')   
-
-    cy.get(selectorsList.dateCloseButton)
-      .click()
-
-    cy.get(selectorsList.submitButton)
-      .eq(0)
-      .click()
-
-    cy.get('body')
-      .should('contain', 'Success')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardGrid).should('be.visible')
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.headerMyInfo).contains('PIM').should('be.visible')    
+    cy.get(selectorsList.firstNameField).clear().type('Felipe')
+    cy.get(selectorsList.middleNameField).clear().type('Recova')
+    cy.get(selectorsList.lastNameField).clear().type('Castilhos')
+    cy.get(selectorsList.genericField).eq(3).clear().type('Recovera')
+    cy.get(selectorsList.genericField).eq(4).clear().type('77742')
+    cy.get(selectorsList.genericField).eq(5).clear().type('ABC_123456-xyz')
+    cy.get(selectorsList.dateField).eq(0).clear().type('2030-12-31')   
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.nationalitySelect).eq(0).click()      
+    cy.get('.oxd-select-dropdown').contains('Brazilian').click()
+    cy.get(selectorsList.maritalStatusSelect).eq(1).click()
+    cy.get('.oxd-select-dropdown').contains('Married').click()
+    cy.get(selectorsList.dateField).eq(1).clear().type('1983-07-27')   
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Success')
   
   })
 
@@ -120,15 +56,9 @@ it('Login - Fail', () => {
 
 cy.visit('/auth/login')
     
-cy.get(selectorsList.usernameField)
-  .type(userData.userFail.username)
-
-cy.get(selectorsList.passwordField)
-  .type(userData.userFail.password)
-
-cy.get(selectorsList.loginButton)
-  .click()
-
+cy.get(selectorsList.usernameField).type(userData.userFail.username)
+cy.get(selectorsList.passwordField).type(userData.userFail.password)
+cy.get(selectorsList.loginButton).click()
 cy.get(selectorsList.wrongCredentialAlert)
 
 }
